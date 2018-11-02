60723705dbf41e08a1bf5e7bda1dd79dbf623cb2
miknhoj <kimjohan86@gmail.com>
2018-11-02 11:18:09 -0400

    initial commit - simple bar chart using d3

---
+++ index.js
@@ -0,0 +1,28 @@
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160]
+
+var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
+
+var svg = d3.select('svg')
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0, svgHeight]);
+
+var barChart = svg.selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("y", function(d) {
+    return svgHeight - d
+  })
+  .attr("height", function(d) {
+    return d;
+  })
+  .attr("width", barWidth - barPadding)
+  .attr("transform", function (d, i) {
+    var translate = [barWidth * i, 0];
+    return "translate("+ translate +")";
+  })
