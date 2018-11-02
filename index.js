2e2dfe10414c3cd3744d7322cc0870f27370b335
miknhoj <kimjohan86@gmail.com>
2018-11-02 15:58:07 -0400

    d3 svg element and line graph

--- index.js
+++ index.js
@@ -34,18 +34,20 @@ var text = svg.selectAll("text")
     return d;
   })
   .attr("y", function(d, i) {
-    return svgHeight - d -2;
+    return svgHeight - d - 2;
   })
   .attr("x", function(d, i) {
-    return barWidth * i;
+    return barWidth * i + (barWidth/2);
   })
-  .attr("fill", "#A64c38");
+  .attr("fill", "#A64c38")
+  .attr("text-anchor", "middle")
+;
 
 // Bar Chart to Scale
-var dataset = [1,2,3,4,5];
+var barChartScale = [1,2,3,4,5];
 
 var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth = (svgWidth / dataset.length);
+var barWidth = (svgWidth / barChartScale.length);
 
 
 var svg = d3.select('.bar-chart-scale')
@@ -53,15 +55,15 @@ var svg = d3.select('.bar-chart-scale')
     .attr("height", svgHeight);
     
 var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset)])
-    .range([0, svgHeight]);
+    .domain([0, d3.max(barChartScale)])
+    .range([0, svgHeight - 10]);
         
 var barChart = svg.selectAll("rect")
-    .data(dataset)
+    .data(barChartScale)
     .enter()
     .append("rect")
     .attr("y", function(d) {
-         return svgHeight - yScale(d) 
+         return svgHeight - yScale(d)
     })
     .attr("height", function(d) { 
         return yScale(d); 
@@ -71,3 +73,49 @@ var barChart = svg.selectAll("rect")
         var translate = [barWidth * i, 0]; 
         return "translate("+ translate +")";
     });
+
+// Axes in DS 
+var axes= [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var svgWidth = 500, svgHeight = 300;
+
+var svg = d3.select('.axes')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var xScale = d3.scaleLinear()
+    .domain([0, d3.max(axes)])
+    .range([0, svgWidth]);
+         
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(axes)])
+    .range([svgHeight, 0]);
+     
+var x_axis = d3.axisBottom().scale(xScale);
+
+var y_axis = d3.axisLeft().scale(yScale);
+         
+svg.append("g")
+    .attr("transform", "translate(50, 10)")
+    .call(y_axis);
+         
+var xAxisTranslate = svgHeight - 20;
+         
+svg.append("g")
+    .attr("transform", "translate(50, " + xAxisTranslate  +")")
+    .call(x_axis);
+
+// SVG Elements
+
+var svgWidth = 600, svgHeight = 500;
+var svg = d3.select(".element")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("class", "svg-container");
+    
+var line = svg.append("line")
+    .attr("x1", 50)
+    .attr("x2", 500)
+    .attr("y1", 400)
+    .attr("y2", 70)
+    .attr("stroke", "red");
