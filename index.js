579b81458f7d6a76784e94461c4907aa49a6118f
miknhoj <kimjohan86@gmail.com>
2018-11-02 13:46:17 -0400

    bar chart scale

--- index.js
+++ index.js
@@ -1,28 +1,73 @@
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160]
+
+// Regular Bar Chart
+var bardata = [80, 100, 56, 120, 180, 30, 40, 120, 160]
 
 var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth = (svgWidth / dataset.length);
+var barWidth = (svgWidth / bardata.length);
 
-var svg = d3.select('svg')
+var svg = d3.select('.bar-chart')
+  // .style('background-color', 'red')
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-var yScale = d3.scaleLinear()
-  .domain([0, d3.max(dataset)])
-  .range([0, svgHeight]);
-
 var barChart = svg.selectAll("rect")
-  .data(dataset)
+  .data(bardata)
   .enter()
   .append("rect")
-  .attr("y", function(d) {
+  .attr("y", function (d) {
     return svgHeight - d
   })
-  .attr("height", function(d) {
+  .attr("height", function (d) {
     return d;
   })
   .attr("width", barWidth - barPadding)
   .attr("transform", function (d, i) {
     var translate = [barWidth * i, 0];
-    return "translate("+ translate +")";
+    return "translate(" + translate + ")";
   })
+// To display the values on top of the bargraph
+var text = svg.selectAll("text")
+  .data(bardata)
+  .enter()
+  .append("text")
+  .text(function(d) {
+    return d;
+  })
+  .attr("y", function(d, i) {
+    return svgHeight - d -2;
+  })
+  .attr("x", function(d, i) {
+    return barWidth * i;
+  })
+  .attr("fill", "#A64c38");
+
+// Bar Chart to Scale
+var dataset = [1,2,3,4,5];
+
+var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
+
+
+var svg = d3.select('.bar-chart-scale')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgHeight]);
+        
+var barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+         return svgHeight - yScale(d) 
+    })
+    .attr("height", function(d) { 
+        return yScale(d); 
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function (d, i) {
+        var translate = [barWidth * i, 0]; 
+        return "translate("+ translate +")";
+    });
